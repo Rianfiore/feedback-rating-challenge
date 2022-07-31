6f3a7a6b347ab10d4ffd6febd8afba3755ca941d
Rian Fiore <74125852+Rianfiore@users.noreply.github.com>
2022-07-31 16:31:29 -0300

    index all components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { Rating } from './rating/rating';
+export { Button } from "./button/button";
+export { Subtitle } from "./subtitle/subtitle";
+export { Title } from "./title/title";
+export { Icon } from "./icon/icon";
+export {Count} from "./count/count"
